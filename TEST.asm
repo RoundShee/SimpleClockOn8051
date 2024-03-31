9c3dfd127c49a2a323c4ba841c7879d8cf28da2a
RoundShee <roundsheep@qq.com>
2024-03-31 21:48:31 +0800

    工作量有点大

--- TEST.asm
+++ TEST.asm
@@ -1,13 +1,25 @@
-;书P250
 ;P1.0--SDA  P1.1--SCL   P1.2--S0    P1.7--RST_L     P3.2--CLKOUT
 ;P1.3--RST	P1.4--RS	P1.5--RW	P1.6--EN		P2--12864
 
 ;RAM空间使用情况:
 ;10H-1DH	PCF8563T初始化相关命令以及时间参数
-;20H-26H	秒中断原始数据读出存放处
-;28H-2FH	38H-3FH	秒个38H 秒十39H	CHAIFEN拆分程序结果存放处
-;30H-33H	12864屏幕使用!!!!!
-;60H		堆栈
+;1EH-1FH	空俩
+;20H-26H	秒中断原始数据读出存放处-再由adjust屏蔽无用位
+;27H		空一
+;28H-2FH	日l	日h	月l	月h 年0 年1	年2	年3
+;30H-33H	12864屏幕-程序参数
+;34H-37H	
+;38H-3FH	秒l	秒h	-	分l	分h	-	时l	时h
+;40H-41H	年3	年2		ASCII
+;42H-43H	年1	年0
+;44H-45H	月h	月l
+;46H-47H	日h	日l
+;48H-49H	时h	时l
+;4AH-4BH	分h	分l
+;4CH-4DH	秒h	秒l
+;4EH		星期0-6		4FH	空
+;50H-5FH	
+;60H		以上堆栈
 
 SDA			BIT		P1.0
 SCL			BIT		P1.1
@@ -37,7 +49,7 @@ START:	MOV		SP,#60H			;堆栈上移-避开变量区域
         LCALL   DELAY
         SETB    RST
         LCALL   ST12864_INT ;初始化12864
-        LCALL   HANZI_WRITE ;调用显示汉字子程序
+        ;LCALL   HANZI_WRITE ;调用显示汉字子程序
 		CLR		P1.7			;ZLG7290B复位
 		LCALL	DELAY
 		SETB	P1.7
@@ -48,10 +60,10 @@ START:	MOV		SP,#60H			;堆栈上移-避开变量区域
 		MOV		12H,#20H		;秒单元
 		MOV		13H,#03H		;分单元
 		MOV		14H,#10H		;小时单元
-		MOV		15H,#30H		;日期单元
-		MOV		16H,#03H		;星期单元
-		MOV		17H,#01H		;月单元
-		MOV		18H,#18H		;年单元
+		MOV		15H,#01H		;日期单元
+		MOV		16H,#01H		;星期单元
+		MOV		17H,#04H		;月单元
+		MOV		18H,#24H		;年单元
 
 		MOV		19H,#00H		;设置分报警
 		MOV		1AH,#00H		;设置小时报警
@@ -67,13 +79,38 @@ START:	MOV		SP,#60H			;堆栈上移-避开变量区域
 		SETB	EA				;允许中断
 		SETB	EX0				;开启外部中断0
 		SETB	IT0				;触发方式-低电平触发
+		MOV		2EH,#0FCH
+		MOV		2FH,#0DAH		;初始化年高两位
 		SJMP	$				;等待中断
 
 ;屏幕内容区域	ROM部分
-TABLE1:	DB      "12大连理工大学12";第一行
+TABLE1:	DB      "2024/0331/21:27";第一行
 TABLE2:	DB      "123456789123456";第三行
 TABLE3:	DB      "222222222222222";第二行
 TABLE4:	DB      "444444444444444";第四行
+
+
+;中断程序
+INC_RCT:
+		MOV		R7,#07H			;读出数据个数
+		MOV		R0,#20H			;目标数据块首地址
+		MOV		R2,#02H			;从器件内部地址
+		MOV		R3,#WSLA_8563
+		MOV		R4,#RSLA_8563	;准备读8563参数
+		LCALL	RDNBYT			;读出数据放置到RAM的20H-26H中
+		LCALL	ADJUST			;调时间调整子程序
+		LCALL	CHAIFEN			;拆分-包含查表
+		LCALL	TIME_WRITE
+		MOV		R7,#08H
+		MOV		R2,#10H
+		MOV		R3,#WSLA_7290
+		JNB		P1.2,YEARS		;使用P1.2控制显示内容  1-显示年月日
+		MOV		R0,#38H			;显示小时分钟秒
+		SJMP	DISP
+YEARS:	MOV		R0,#28H			;显示年月日
+DISP:	LCALL	WRNBYT			;调用ZLG7290B显示
+		JNB		P3.2,$
+		RETI
 
 ;模块初始化程序
 ST12864_INT:                ;模块初始化程序
@@ -127,10 +164,10 @@ HANZI_LOOP:
 		RET
 
 
-;写时间子程序
+;写时间至LCD子程序
+;入口参数
+;SONG起始位置,XUNHUAN多少位,R0源始地址
 TIME_WRITE:					;魔改程序
-		PUSH	00H
-		MOV		SONG,#80H	;第一行起始
 		LCALL	SEND_ML
 		MOV		XUNHUAN,#8	;全部显示
 		MOV		R0,#38H	;秒
@@ -141,7 +178,6 @@ TIMWRT_LOOP:
 		LCALL	SEND_SJ
 		INC		R0
 		DJNZ	XUNHUAN,TIMWRT_LOOP
-		POP		00H
 		RET
 
 ;发送数据子程序
@@ -156,72 +192,70 @@ SEND_SJ:
 ;-------------------------------------
 ;下面是关于ST12864的内容 		
 
-;中断程序
-INC_RCT:
-		MOV		R7,#07H			;读出数据个数
-		MOV		R0,#20H			;目标数据块首地址
-		MOV		R2,#02H			;从器件内部地址
-		MOV		R3,#WSLA_8563
-		MOV		R4,#RSLA_8563	;准备读8563参数
-		LCALL	RDNBYT			;读出数据放置到RAM的20H-26H中
-		LCALL	ADJUST			;调时间调整子程序
-		LCALL	CHAIFEN			;拆分-包含查表??
-								;将20H-26H中的参数分别存放到28H-2FH年月日, 38H-3FH时分秒	两个8段
-		LCALL	TIME_WRITE
-		MOV		R7,#08H
-		MOV		R2,#10H
-		MOV		R3,#WSLA_7290
-		JNB		P1.2,YEARS		;使用P1.2控制显示内容  1-显示年月日
-		MOV		R0,#38H			;显示小时分钟秒
-		SJMP	DISP
-YEARS:	MOV		R0,#28H			;显示年月日
-DISP:	LCALL	WRNBYT			;调用ZLG7290B显示
-		JNB		P3.2,$
-		RETI
-
 ;拆分子程序-将RAM空间20H-26H查表拆分送入28H-2FH,38H-3FH
-CHAIFEN:
+CHAIFEN:						;增加ASCII存储
 		PUSH	PSW
 		PUSH	ACC
 		PUSH	03H				;R3
 		PUSH	04H
+		PUSH	05H
+		PUSH	06H
 		MOV		A,20H			;取秒
 		LCALL	CF				;拆分,查表在R4(H),R3中
 		MOV		38H,R3
 		MOV		39H,R4
+		MOV		4DH,R5
+		MOV		4CH,R6			;ASCII
 		MOV		3AH,#02H		;送分隔符-
+
 		MOV		A,21H			;取分钟 参数
 		LCALL	CF
 		MOV		3BH,R3
 		MOV		3CH,R4
+		MOV		4BH,R5
+		MOV		4AH,R6			;ASCII
 		MOV		3DH,#02H
+
 		MOV		A,22H			;取小时 参数
 		LCALL	CF
 		MOV		3EH,R3
 		MOV		3FH,R4
-		MOV		A,23H			;取日期参数
+		MOV		49H,R5
+		MOV		48H,R6			;ASCII
+
+		MOV		A,23H			;取日参数
 		LCALL	CF
-		MOV		A,R3
-		ORL		A,#01H
-		MOV		R3,A
+		;MOV		A,R3		;拆分后被转码
+		;ORL		A,#01H		;这里不对
+		;MOV		R3,A
 		MOV		28H,R3
 		MOV		29H,R4
+		MOV		47H,R5
+		MOV		46H,R6			;ASCII
+
 		MOV		A,25H			;取月参数
 		LCALL	CF
-		MOV		A,R3
-		ORL		A,#01H
-		MOV		R3,A
+		;MOV		A,R3
+		;ORL		A,#01H
+		;MOV		R3,A
 		MOV		2AH,R3
 		MOV		2BH,R4
+		MOV		45H,R5
+		MOV		44H,R6			;ASCII
+
 		MOV		A,26H			;取年参数
 		LCALL	CF
-		MOV		A,R3
-		ORL		A,#01H
-		MOV		R3,A
+		;MOV		A,R3
+		;ORL		A,#01H
+		;MOV		R3,A
 		MOV		2CH,R3
 		MOV		2DH,R4
-		MOV		2EH,#0FCH
-		MOV		2FH,#0DAH
+		MOV		43H,R5
+		MOV		42H,R6			;ASCII
+		;MOV		2EH,#0FCH		;0
+		;MOV		2FH,#0DAH		;2后期得改
+		POP		06H
+		POP		05H
 		POP		04H
 		POP		03H
 		POP		ACC
@@ -233,15 +267,21 @@ CF:		PUSH	02H
 		PUSH	DPH
 		PUSH	DPL
 		MOV		DPTR,#LEDSEG
-		MOV		R2,A
-		ANL		A,#0FH
-		MOVC	A,@A+DPTR
-		MOV		R3,A
+		MOV		R2,A			;备份A
+		ANL		A,#0FH			;取低位
+		ADD		A,'0'
+		MOV		R5,A
+		SUB		A,'0'
+		MOVC	A,@A+DPTR		;查表
+		MOV		R3,A			;到R3
 		MOV		A,R2
 		SWAP	A
 		ANL		A,#0FH
+		ADD		A,'0'
+		MOV		R6,A
+		SUB		A,'0'
 		MOVC	A,@A+DPTR
-		MOV		R4,A
+		MOV		R4,A			;到R4
 		POP		DPL
 		POP		DPH
 		POP		02H
@@ -291,6 +331,7 @@ DELAY1:	MOV		R1,#00H
 ;R2从器件内部子地址
 ;R3外围地址
 ;相关子程序WRBYT,STOP,CACK,STA
+;个人表述：R3.W(R2)<---R7---R0
 WRNBYT:	PUSH	PSW
 		PUSH	ACC
 WRADD:	MOV		A,R3			;取外围地址
@@ -319,6 +360,8 @@ WRDA:	MOV		A,@R0
 ;R0目标数据块首地址
 ;R2从器件内部首地址
 ;R3器件地址-写,R4器件地址-读
+;个人表述：R3.W<-----
+;			R4.R(R2)---R7--->R0
 RDNBYT:	PUSH	PSW
 		PUSH	ACC
 RDADD1:	LCALL	STA
@@ -347,6 +390,7 @@ ACK:	LCALL	MNACK
 		INC		R0
 		SJMP	RDN
 
+;最底层iic
 ;发送启动信号s
 STA:
 		SETB	SDA
